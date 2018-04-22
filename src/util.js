54de1908e2bed884ca3f307c2a5474ae37570a47
Ayaz <ayazzali@mail.ru>
2018-04-22 18:44:18 +0300

    util: class DB - for db tables name

--- src/util.js
+++ src/util.js
@@ -4,6 +4,7 @@ const _REST = "http://192.168.43.252:3000"
   , _users = "users"
 
 export function RestFetch(route = 'posts/1/comments', action = "GET", bodyThatIWillStringify) {
+  console.log(route);
   return fetch(_REST + '/' + route, {
     headers: { 'Content-Type': 'application/json' },
     method: action,
@@ -11,6 +12,7 @@ export function RestFetch(route = 'posts/1/comments', action = "GET", bodyThatIW
   })
     .then(e => e.json())
     .then(res => {
+      console.log('[from RestFetch] V')
       console.log(res);//todo delete
       return res;
     })
@@ -24,4 +26,9 @@ export let screens = {
   _CategoryFull: "_CategoryFull",
   _MainTeacher: "_MainTeacher",
   _TeacherStat: "_TeacherStat",
+}
+export const DB={
+  usersCategories:'usersCategories',
+  categories:'categories',
+  users:'users',
 }
